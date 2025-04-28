6f6ff15cd49fdc8c79d2f50495ab750f5bbfe47c
fatihozcevikk <22120205063@ismu.edu.tr>
2025-04-28 17:19:43 +0300

    bolum10.2 eklendi

---
+++ lib/bolum10.2.dart
@@ -0,0 +1,135 @@
+/*  ---------- AÇIKLAMA -----------------
+
+Bu bölümde Ekranlar arası geçite veri gönderme ve veri alma ile
+alakalı basit bir örnek yaptk,
+
+önce ikinci ekrana bir String parametresi yolladık, bunu ikinci ekran
+kullanıd
+
+ikinci ekrandanki bir parametreyi de birinci ekrana yollamış olduk.
+
+
+
+ */
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Bolum10_1());
+}
+
+class Bolum10_1 extends StatefulWidget {
+  const Bolum10_1({super.key});
+
+  @override
+  State<Bolum10_1> createState() => _Bolum10_1State();
+}
+
+class _Bolum10_1State extends State<Bolum10_1> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Sayfa1(),
+    );
+  }
+}
+
+class Sayfa1 extends StatelessWidget {
+  const Sayfa1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreenAccent,
+        title: Text("EKRAN 1"),
+      ),
+      body: Center(child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepOrange
+          ),
+          onPressed: () async {
+            final sonuc = await Navigator.of(context).push<String>(
+                MaterialPageRoute(builder: (context) => Sayfa2("Görseli beğendiniz mi ?"),
+                )
+            );
+            print(sonuc);
+          },
+          child: Text(
+            "2. EKRANA GEÇ",
+            style: TextStyle(
+                color: Colors.white
+            ),
+          )
+        ),
+
+
+      ),
+
+
+    );
+  }
+}
+
+class Sayfa2 extends StatelessWidget {
+
+  final String mesaj;
+  const Sayfa2(String this.mesaj, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreenAccent,
+        title: Text("EKRAN 2"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 35,
+            ),
+            Container(
+                height: 400,
+                child: Image.network("https://st.depositphotos.com/67903508/59949/i/950/depositphotos_599493982-stock-photo-vertical-shot-bosphorus-bridge-sunset.jpg")
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Text(mesaj),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange
+                ) ,
+                onPressed: (){
+                  Navigator.of(context).pop("evet");
+                } ,
+                child: Text(
+                  "EVET",
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                )
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepOrange
+                ) ,
+                onPressed: (){
+                  Navigator.of(context).pop("hayır");
+                } ,
+                child: Text(
+                  "HAYIR",
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
