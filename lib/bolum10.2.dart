a1c6b84f78b635b50c9d45eaad214665a6eca945
fatihozcevikk <22120205063@ismu.edu.tr>
2025-04-28 23:21:02 +0300

    bolum10.2 ye willPopScope eklendi

--- lib/bolum10.2.dart
+++ lib/bolum10.2.dart
@@ -8,6 +8,8 @@ kullanıd
 
 ikinci ekrandanki bir parametreyi de birinci ekrana yollamış olduk.
 
+Ayrıca willPopScope widget'ının nasıl kullanılması gerektiğine basit bir örnek yapmış oldum.
+
 
 
  */
@@ -78,54 +80,60 @@ class Sayfa2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreenAccent,
-        title: Text("EKRAN 2"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 35,
-            ),
-            Container(
-                height: 400,
-                child: Image.network("https://st.depositphotos.com/67903508/59949/i/950/depositphotos_599493982-stock-photo-vertical-shot-bosphorus-bridge-sunset.jpg")
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Text(mesaj),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepOrange
-                ) ,
-                onPressed: (){
-                  Navigator.of(context).pop("evet");
-                } ,
-                child: Text(
-                  "EVET",
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                )
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepOrange
-                ) ,
-                onPressed: (){
-                  Navigator.of(context).pop("hayır");
-                } ,
-                child: Text(
-                  "HAYIR",
-                  style: TextStyle(
-                      color: Colors.white
-                  ),
-                )
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        print("pop edilecek");
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreenAccent,
+          title: Text("EKRAN 2"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 35,
+              ),
+              Container(
+                  height: 400,
+                  child: Image.network("https://st.depositphotos.com/67903508/59949/i/950/depositphotos_599493982-stock-photo-vertical-shot-bosphorus-bridge-sunset.jpg")
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(mesaj),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepOrange
+                  ) ,
+                  onPressed: (){
+                    Navigator.of(context).maybePop("evet");
+                  } ,
+                  child: Text(
+                    "EVET",
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  )
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepOrange
+                  ) ,
+                  onPressed: (){
+                    Navigator.of(context).maybePop("hayır");
+                  } ,
+                  child: Text(
+                    "HAYIR",
+                    style: TextStyle(
+                        color: Colors.white
+                    ),
+                  )
+              ),
+            ],
+          ),
         ),
       ),
     );
