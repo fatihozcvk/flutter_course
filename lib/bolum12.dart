39a19fa5486c16bad511e1738161ed349d29f037
fatihozcevikk <22120205063@ismu.edu.tr>
2025-05-01 01:01:09 +0300

    bolum 12 asenkron ve ss eklendi

---
+++ lib/bolum12.dart
@@ -0,0 +1,163 @@
+/*  ---------- AÇIKLAMA -----------------
+
+Bu bölümde önceki örnekteki versiyondan farklı olarak :
+
+sayfalar arası geçişte init fonksiyon ile başlatılan asenkron
+bir fonksiyon çalıştırdım. Stateful ile asenkron ilişkisini
+state yardımı ile basit şekilde uygulamış oldum.
+
+
+ */
+
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(Bolum12());
+}
+
+class Bolum12 extends StatefulWidget {
+  const Bolum12({super.key});
+
+  @override
+  State<Bolum12> createState() => _Bolum12State();
+}
+
+class _Bolum12State extends State<Bolum12> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Sayfa_1(),
+    );
+  }
+}
+
+class Sayfa_1 extends StatefulWidget {
+  const Sayfa_1({super.key});
+
+  @override
+  State<Sayfa_1> createState() => _Sayfa_1State();
+}
+
+class _Sayfa_1State extends State<Sayfa_1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+            "SAYFA 1",
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.indigo
+            ),
+            onPressed: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => Sayfa_2(),
+                      )
+                  );
+            },
+            child: Text(
+                "ASENKRON SAYFASINA GEÇİŞ",
+              style: TextStyle(
+                color: Colors.white
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
+
+// SAYFA 2 --------------------------------------------------------------------------------
+
+class Sayfa_2 extends StatefulWidget {
+  const Sayfa_2({super.key});
+
+  @override
+  State<Sayfa_2> createState() => _Sayfa_2State();
+
+}
+
+class _Sayfa_2State extends State<Sayfa_2> {
+
+
+  bool? yukleniyor = false;
+  String? resimYolu = "";
+
+  @override
+  void initState() {
+    yukle();
+    super.initState();
+  }
+
+  Future<void> yukle() async {
+    setState(() {
+      yukleniyor = true;
+      resimYolu = "";
+    });
+
+    try{
+      await Future.delayed(Duration(seconds: 2));
+      setState(() {
+        resimYolu = "https://picsum.photos/250";
+      });
+    }
+    catch(e){
+      print(e);
+    }
+    finally{
+      setState(() {
+        yukleniyor = false;
+      });
+    }
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(yukleniyor!){
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(
+            "SAYFA 2",
+            style: TextStyle(
+                color: Colors.white
+            ),
+          ),
+        ),
+        body: Center(
+            child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    else{
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text(
+            "SAYFA 2",
+            style: TextStyle(
+                color: Colors.white
+            ),
+          ),
+        ),
+        body: Center(child: Image.network(resimYolu!)),
+      );
+
+    }
+  }
+
+
+}
+
